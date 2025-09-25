a6d688c906e3e835fad75623c4af072e9bf341be
Muchiri Mwangi <muchirimwangi09@gmail.com>
2025-09-25 21:07:20 +0300

    feat: exclude unnecessary fields from getListings response for improved performance

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1063,6 +1063,7 @@ export const getListings = async (req, res) => {
     // { verified: 'Verified',  isActive: true }
     const listings = await listingModel
       .find()
+      .select('-aiFindings -promoteUntil -inventory -shipingOptions -expiresAt -AgreedToTerms -analytics -createdAt -updatedAt -__v')
       .populate('seller.sellerId', 'personalInfo.fullname personalInfo.phone')
       .lean();
     
