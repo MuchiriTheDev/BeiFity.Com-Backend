c5af32edcc294a322693001e83b05c584ce6422c
Muchiri Mwangi <muchirimwangi09@gmail.com>
2025-10-16 23:53:29 +0300

    mREMBO

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 const corsOptions = {
-  origin:  'https://www.beifity.com',
+  origin:  '*',
   methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE', 'PUT'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'token'],
