e72ce87171d075942931b9310f741f11ad7eb6ad
Muchiri Mwangi <muchirimwangi09@gmail.com>
2025-10-19 12:48:07 +0300

    one

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 const corsOptions = {
+  origin:  '*',
   methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE', 'PUT'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'token'],
