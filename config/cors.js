8e8e7bf19ecc0688d459cff7b654fa15f5e0d690
Muchiri Mwangi <muchirimwangi09@gmail.com>
2025-11-06 22:43:13 +0300

    Fast change

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 const corsOptions = {
-  origin:  '*',
+  origin:  'https://www.beifity.com',
   methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE', 'PUT'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'token'],
